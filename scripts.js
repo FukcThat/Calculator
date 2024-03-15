78216455603a1bb1f43885542e44100e86b12c07
FukcThat <63652e636f6465@gmail.com>
2024-03-16 00:21:12 +0100

    FEAT: Operators now replace each other if clicked in succession

--- scripts.js
+++ scripts.js
@@ -1,20 +1,32 @@
-const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".number, .operator");
 
-// Make sure everything is loaded up first, add EventListener to each button, get the value, handle "." and 0, stich it together
+// Make sure everything is loaded up first
 document.addEventListener("DOMContentLoaded", function () {
+  const display = document.getElementById("display");
+
+  // Add event listener to each button
   buttons.forEach(function (button) {
     button.addEventListener("click", function () {
-      var value = button.textContent;
+      const value = button.textContent;
 
+      // Clear the display if the current value is "0" and the clicked button is not "."
       if (display.value === "0" && value !== ".") {
         display.value = "";
       }
 
+      // If the display value is "0" and "." is clicked, add "0" before the "."
       if (display.value === "" && value === ".") {
         display.value = "0" + value;
       } else {
         display.value += value;
+      }
+
+      // Check if the clicked value is an operator and the last character in the display value is also an operator
+      if (
+        value.match(/[+\-*/]/) &&
+        display.value.slice(-2, -1).match(/[+\-*/]/)
+      ) {
+        display.value = display.value.slice(0, -2) + value;
       }
     });
   });
