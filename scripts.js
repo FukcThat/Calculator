14586ac1b3e580dcf2e4bd6610536bf7bbc52dfc
FukcThat <63652e636f6465@gmail.com>
2024-03-17 12:51:25 +0100

    FIX: Fixed possibility of multiple decimal points in numbers between operators

--- scripts.js
+++ scripts.js
@@ -1,45 +1,43 @@
 const buttons = document.querySelectorAll(".number, .operator");
 
-// Make sure everything is loaded up first
 document.addEventListener("DOMContentLoaded", function () {
   const display = document.getElementById("display");
 
   // Add event listener to each button
   buttons.forEach(function (button) {
     button.addEventListener("click", function () {
+      // Get the value of the clicked button
       const value = button.textContent;
 
-      // Clear the display if the current value is "0" and the clicked button is not "."
+      // Clear display if current value is "0" and clicked button is not "."
       if (display.value === "0" && value !== ".") {
         display.value = "";
       }
 
-      // If the display value is "0" and "." is clicked, add "0" before the "."
-      if (display.value === "" && value === ".") {
-        display.value = "0" + value;
-      } else {
-        display.value += value;
+      // Handle decimal points
+      if (value === ".") {
+        // Split the display value into number strings
+        const numberStrings = display.value.split(/[+\-*/]/);
+        const lastNumberString = numberStrings[numberStrings.length - 1];
+        // Ignore multiple decimals within number string
+        if (lastNumberString.includes(".")) {
+        }
+        // Add "0" before decimal
+        if (display.value === "" || display.value.slice(-1).match(/[+\-*/]/)) {
+          display.value += "0";
+        }
+      } else if (value.match(/[+\-*/]/)) {
+        // Replace Operators
+        if (display.value.slice(-1).match(/[+\-*/]/)) {
+          display.value = display.value.slice(0, -1) + value;
+          return;
+        }
       }
-
-      // Check if the clicked value is an operator and the last character in the display value is also an operator
-      if (
-        value.match(/[+\-*/]/) &&
-        display.value.slice(-2, -1).match(/[+\-*/]/)
-      ) {
-        display.value = display.value.slice(0, -2) + value;
-      }
+      // Display clicked values as concat
+      display.value += value;
     });
   });
 });
-
-// let num1 = 3;
-// let operator = "+";
-// let num2 = 5;
-
-// Operate Function
-// function operate(operator, num1, num2) {
-//   add();
-// }
 
 // add
 
